cb3a4dd1286128294f42017cbb53376df0fe3104
ghxops <51935312+ghxops@users.noreply.github.com>
2019-06-21 13:56:12 -0700

    demo

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-resource "aws_elb" "web-elb" {
+      resource "aws_elb" "web-elb" {
   name = "terraform-example-elb"
 
   # The same availability zone as our instances
   availability_zones = ["${split(",", var.availability_zones)}"]
 
   listener {
-    instance_port     = 80
+instance_port     = 80
     instance_protocol = "http"
     lb_port           = 80
     lb_protocol       = "http"
@@ -19,7 +19,7 @@ resource "aws_elb" "web-elb" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 3
-    target              = "HTTP:80/"
+      target              = "HTTP:80/"
     interval            = 30
   }
 }
