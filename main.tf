24aa1ed662652640a54691a5ec2eec085eb6cc3c
ghxops <51935312+ghxops@users.noreply.github.com>
2019-06-21 13:59:32 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ instance_port     = 80
 
   health_check {
     healthy_threshold   = 2
-    unhealthy_threshold = 2
+      unhealthy_threshold = 2
     timeout             = 3
       target              = "HTTP:80/"
     interval            = 30
