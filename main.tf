23e4e430b3a98225c6ae71da540232da6fee8d40
ghxops <51935312+ghxops@users.noreply.github.com>
2019-06-21 20:59:53 +0000

    style: format with terraform fmt

--- main.tf
+++ main.tf
@@ -2,24 +2,24 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-      resource "aws_elb" "web-elb" {
+resource "aws_elb" "web-elb" {
   name = "terraform-example-elb"
 
   # The same availability zone as our instances
   availability_zones = ["${split(",", var.availability_zones)}"]
 
   listener {
-instance_port     = 80
+    instance_port     = 80
     instance_protocol = "http"
     lb_port           = 80
-       lb_protocol       = "http"
+    lb_protocol       = "http"
   }
 
   health_check {
     healthy_threshold   = 2
-      unhealthy_threshold = 2
+    unhealthy_threshold = 2
     timeout             = 3
-      target              = "HTTP:80/"
+    target              = "HTTP:80/"
     interval            = 30
   }
 }
